4fc0e9fce72b3280bcb788ec83c81da02eb077bb
Czaja <dczajka1@gmail.com>
2024-01-07 22:30:23 +0100

    small changes in code structure

--- antycaptcha2.py
+++ antycaptcha2.py
@@ -1,8 +1,6 @@
 from time import sleep
 
-import keyboard
-import pyautogui
-from bot_useful_functions import click_on_position, type_sentence, press_keyboard_sequence
+from bot_useful_functions import *
 from bs4 import BeautifulSoup
 import httpx
 from pynput import mouse
@@ -19,7 +17,6 @@ class CaptchaBot:
         self.button1 = 'b1_button.png'
         self.button2 = 'b2_button.png'
 
-
     def items(self) -> list:
         return [
             self.text_field,
@@ -28,7 +25,6 @@ class CaptchaBot:
             self.button1,
             self.button2
         ]
-
 
     def get_code_tags(self) -> tuple:
         """ Given response from site parses to find <code> tag.
@@ -41,64 +37,30 @@ class CaptchaBot:
 
         return sentence, field_to_fill_with_sentence, button_to_press, expected_output
 
-    def locate_item(self, item_path: str):
-        """ Locate on open website window an item.
-        Item ( a button or text field ) needs to be a path to image file,
-        in order to be located on the screen """
-        try:
-            return pyautogui.locateOnScreen(item_path, confidence=0.8)
-        except pyautogui.ImageNotFoundException as error:
-            return error
-
-
     def click_on_item(self, item_path: str):
+        """ Clicks on location of given item """
         try:
-            x, y, *_ = self.locate_item(item_path)
+            x, y, *_ = locate_item(item_path)
             self.mouse_controller.position = x, y
             sleep(.2)
             click_on_position(self.mouse_controller, x, y)
         except pyautogui.ImageNotFoundException:
             print('see nothing')
 
-
-
-
     def final_step(self):
         """ Checks if solution is correct"""
-        x, y, *_ = self.locate_item(self.solution)
+        x, y, *_ = locate_item(self.solution)
         click_on_position(self.mouse_controller, x, y)
 
-        if self.locate_item(self.good_answer) is not None:
+        if locate_item(self.good_answer) is not None:
             keyboard.press('q')
             keyboard.release('q')
             print('End of program. Good answer met.')
 
 
-
-
-    def click_mouse_with_sequence(self, seq: list|tuple[str]):
-        """ Sequence consists of reference paths of items"""
-
-        sleep(.2)
-        for item in seq:
-            # if given image not found on screen the output is None
-            try:
-                x, y, *_ = self.locate_item(item)
-                print(f'x:{x} , y:{y}')
-                self. mouse_controller.position = (x, y)
-                sleep(0.5)
-                click_on_position(self.mouse_controller, x, y)
-
-            except pyautogui.ImageNotFoundException:
-                print('see nothing')
-        else:
-            self.final_step()
-
-
-
 if __name__ == '__main__':
-
-    bot = CaptchaBot('https://antycaptcha.amberteam.pl/exercises/exercise2?seed=07a769c2-3a00-4a48-a07b-4b2b6fe93488')
+    seed = 'c265819e-6ed3-4ea6-9753-5a554d5a6481'
+    bot = CaptchaBot(f'https://antycaptcha.amberteam.pl/exercises/exercise2?seed={seed}')
 
     prompt, field, button, output = bot.get_code_tags()
     if button.lower().strip() == 'b1':
@@ -107,12 +69,10 @@ if __name__ == '__main__':
         button = bot.button2
 
     sleep(2)
-    bot.locate_item(bot.text_field)
+    # locate_item(bot.text_field)
     bot.click_on_item(bot.text_field)
     press_keyboard_sequence('ctrl', 'a')
     pyautogui.press('del')
     type_sentence(prompt)
     bot.click_on_item(button)
     bot.final_step()
-
-
--- bot_useful_functions.py
+++ bot_useful_functions.py
@@ -12,13 +12,22 @@ def display_position():
         print(f'{x} , {y}')
 
 
+def locate_item(item_path: str):
+    """ Locate on open website window an item.
+        Item ( a button or text field ) needs to be a path to image file,
+        in order to be located on the screen """
+    try:
+        return pyautogui.locateOnScreen(item_path, confidence=0.8)
+    except pyautogui.ImageNotFoundException as error:
+        return error
+
+
 def click_on_position(controller: mouse.Controller, x: int, y: int, count: int = 1):
     controller.position = (x+8, y+8)
     for num in range(count):
         controller.press(mouse.Button.left)
         time.sleep(0.01)
         controller.release(mouse.Button.left)
-
 
 ### keyboard ###
 def press_keyboard_sequence(*keys):
